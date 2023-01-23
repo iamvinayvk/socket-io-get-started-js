da6d28d3b06104de0927b6884cbd8bcb28e56daf
Vinay Kushwaha <iamvinayvk@gmail.com>
2023-01-23 16:35:23 +0530

    fix: updated client socket url

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import "./App.css";
 
 const App = () => {
-  const socket = io("http://localhost:3000");
+  const socket = io("https://socket-io-get-started-js-server.onrender.com/");
   const [username, setUsername] = React.useState(null);
   const [name, setName] = React.useState(null);
   const [message, setMessage] = React.useState("");
